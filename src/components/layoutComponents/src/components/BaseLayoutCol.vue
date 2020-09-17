d281fe4472946173ffced9f090049535db29580c
silver ou <964973099@qq.com>
2020-09-17 12:27:10 +0800

    -add layout components

---
+++ src/components/layoutComponents/src/components/BaseLayoutCol.vue
@@ -0,0 +1,28 @@
+<template>
+  <div
+    :class="'base-layout-col base-layout-col-' + span + ' ' + offsetClass"
+  >
+    <slot><div></div></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    span: { type: Number, default: 0 },
+    offset: { type: Number, default: 0 }
+  },
+  computed: {
+    offsetClass () {
+      if (this.offset !== 0) {
+        return 'base-layout-col-offset-' + this.offset
+      }
+      return ''
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import '../styles/index.scss';
+</style>
---
+++ src/components/layoutComponents/src/components/BaseLayoutRow.vue
@@ -0,0 +1,32 @@
+<template>
+  <div :class="'base-layout-row base-layout-row ' + spacingClass + ' ' + justifyClass">
+    <slot><base-layout-col></base-layout-col></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    spacing: { type: Boolean, default: false },
+    justify: { type: String, default: '' }
+  },
+  computed: {
+    spacingClass () {
+      if (this.spacing) {
+        return 'base-layout-row-spacing'
+      }
+      return ''
+    },
+    justifyClass () {
+      if (this.justify !== '') {
+        return 'is-justify-' + this.justify
+      }
+      return ''
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import '../styles/index.scss';
+</style>
